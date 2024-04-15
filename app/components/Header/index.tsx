6984a4e57227591bf5fa631e3af7123df34e5e1f
Anatoliy Shumov <anatoliyshumov@MacBook-Air-Anatoliy.local>
2024-04-15 19:50:51 +0300

    add changes

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,32 +1,48 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
 import {AvatarCircle} from '../AvatarCircle';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../store/store.ts';
+import RocketYellow from '../../assets/images/components/RocketYellow.tsx';
 
 const CustomHeader = ({title, onLeftPress, onRightPress}) => {
   const userData = useSelector((state: RootState) => state.user);
   return (
-    <View style={styles.header}>
-      <TouchableOpacity onPress={onLeftPress}>
-        <Text>Ліво</Text>
-      </TouchableOpacity>
-      <Text style={styles.title}>{title}</Text>
-      <TouchableOpacity onPress={onRightPress}>
-        <AvatarCircle
-          url={userData.avatarUser}
-          size={50}
-          defaultImage={false}
-          borderWidth={2}
-          name={''}
-          borderColor="rgba(110, 91, 125, 1)"
-        />
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={onLeftPress}>
+          <RocketYellow />
+        </TouchableOpacity>
+        <Text style={styles.title}>{title}</Text>
+        <TouchableOpacity onPress={onRightPress}>
+          <AvatarCircle
+            url={userData.avatarUser}
+            size={45}
+            defaultImage={false}
+            borderWidth={2}
+            name={''}
+            borderColor="rgba(110, 91, 125, 1)"
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: '#292F3D', // Same as header background
+    // For Android, make sure the status bar color matches
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
   header: {
     flexDirection: 'row',
     height: 90,
