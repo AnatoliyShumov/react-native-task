9787f4bb19dcb193219200bf085658e753d001d3
Anatoliy Shumov <anatoliyshumov@MacBook-Air-Anatoliy.local>
2024-04-16 11:21:45 +0300

    add changes

--- app/screens/TableView/index.tsx
+++ app/screens/TableView/index.tsx
@@ -196,7 +196,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 22,
+    marginTop: 40,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   modalView: {
